4279eea4059a2d9775823a4863f6d061cc40f1ea
Martijn Vermaat <martijn@vermaat.name>
2014-10-01 21:38:53 +0200

    On-the-fly static rendering of IPython notebooks
    If enabled (via the ipython_notebook.render configuration setting),
    visiting the blob view on any .ipynb file shows a 'View' button in the
    action bar. Clicking it triggers a call to IPython nbconvert and the
    result is shown verbatim. IPython (>= 1.0) must be installed on the
    server.
    
    Note that this is a very ad-hoc patch. For example, rendered notebooks
    are not cached meaning that performance may be suboptimal.

--- config/routes.rb
+++ config/routes.rb
@@ -214,6 +214,7 @@ Gitlab::Application.routes.draw do
         get :diff, on: :member
       end
       resources :raw,       only: [:show], constraints: {id: /.+/}
+      resources :ipython_notebook, only: [:show], constraints: {id: /.+/}
       resources :tree,      only: [:show], constraints: {id: /.+/, format: /(html|js)/ }
       resources :edit_tree, only: [:show, :update], constraints: { id: /.+/ }, path: 'edit' do
         # Cannot be GET to differentiate from GET paths that end in preview.
